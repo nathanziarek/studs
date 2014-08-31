11796aa229b1be5b5c8359f937006045ce525647
Nathan Ziarek <nziarek@directs.com>
2014-08-30 21:28:45 -0500

    Updating tests and doc build

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@ var gulp = require('gulp'),
     less = require('gulp-less');
     prefix = require('gulp-autoprefixer');
 
-  gulp.task( 'default', ['build-less', 'auto-prefix'])
+gulp.task( 'default', ['build-less', 'auto-prefix'])
 
 gulp.task( 'build-less', function(cb){
-  gulp.src('studs*.less')
+  gulp.src('docs/**/*.less')
     .pipe(less())
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('docs'))
     .on("end", cb);
 });
 
 gulp.task( 'auto-prefix', ['build-less'], function(cb){
-  gulp.src('build/*.css')
+  gulp.src('docs/**/*.css')
     .pipe(prefix(["last 1 version", "> 1%", "ie 8", "ie 7"], { cascade: true }))
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('docs'))
     .on("end", cb);
 });
