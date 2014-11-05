5c7192728420db196717ef0dbb7417db851345f0
Nathan Ziarek <nate.ziarek@directsupply.com>
2014-11-05 07:55:16 -0600

    feat(Animations): Buttons now smoothly animate from transition to transition

--- test/quiz/buttons.js
+++ test/quiz/buttons.js
@@ -1,33 +1,39 @@
 casper.thenOpen( 'docs/buttons/test.html' );
 
 var buttonStyles = [
-    { selector: '.grey0', title: 'Button - Grey 0' },
-    { selector: '.grey25', title: 'Button - Grey 25' },
-    { selector: '.grey50', title: 'Button - Grey 50' },
-    { selector: '.grey75', title: 'Button - Grey 75' },
-    { selector: '.grey100', title: 'Button - Grey 100' },
-    { selector: '.color1', title: 'Button - Color 01' },
-    { selector: '.color2', title: 'Button - Color 02' },
-    { selector: '.color3', title: 'Button - Color 03' },
-    { selector: '.color4', title: 'Button - Color 04' },
+    { selector: 'grey0', title: 'Button - Grey 0' },
+    { selector: 'grey25', title: 'Button - Grey 25' },
+    { selector: 'grey50', title: 'Button - Grey 50' },
+    { selector: 'grey75', title: 'Button - Grey 75' },
+    { selector: 'grey100', title: 'Button - Grey 100' },
+    { selector: 'color1', title: 'Button - Color 01' },
+    { selector: 'color2', title: 'Button - Color 02' },
+    { selector: 'color3', title: 'Button - Color 03' },
+    { selector: 'color4', title: 'Button - Color 04' },
 ];
 
-var curIndex = 0;
+var states = ["Hover", "Active", "Focus", ""];
 
-casper.repeat(buttonStyles.length, function() {
-    buttonStyle = buttonStyles[curIndex];
-    casper.then(function(){
-        phantomcss.screenshot(buttonStyle.selector, buttonStyle.title);
+
+var curIndex = 0;
+var steIndex = -1;
+
+casper.repeat(states.length, function() {
+    curIndex = 0;
+    casper.repeat(buttonStyles.length, function() {
+        buttonStyle = buttonStyles[curIndex];
+        casper.then(function(){
+            name = buttonStyle.title;
+            selc = ".cell-" + buttonStyle.selector;
+            if(states[steIndex]) {
+                name += " - " + states[steIndex];
+                selc += states[steIndex].toLowerCase()
+            }
+            phantomcss.screenshot(selc, name);
+        });
+        curIndex++;
     });
-    casper.then(function(){
-        this.mouse.move(buttonStyle.selector);
-        phantomcss.screenshot(buttonStyle.selector, buttonStyle.title + ' - Hover');
-    });
-    casper.then(function(){
-        this.mouse.down(buttonStyle.selector);
-        phantomcss.screenshot(buttonStyle.selector, buttonStyle.title + ' - Down');
-    });
-    curIndex++;
+    steIndex++;
 });
 
 casper.test.done();
